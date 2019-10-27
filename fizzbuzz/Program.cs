b26258b458b6d1cd65a3b3b64a413516ae8321f1
muhedin <muhedin1998@gmail.com>
2019-10-27 22:36:23 +0100

    Add project files.

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite do kojeg broja zelite da idete: ?");
+            int brojac = int.Parse(Console.ReadLine());
+            for(int i =0; i<= brojac; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0 && i != 0)
+                {
+                    Console.WriteLine("Fiiz-Buzz");
+                }
+                else if (i % 5 == 0 && i!=0)
+                    Console.WriteLine("Buzzz");
+                else if (i % 3 == 0 && i!=0)
+                    Console.WriteLine("Fizz");
+                else
+                    Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        } 
+    }
+}
